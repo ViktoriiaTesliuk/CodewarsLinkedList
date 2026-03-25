6d4afde8553f5af1622f754eb2645df7b7f5382b
ViktoriiaTesliuk <tesliuk.pn@ucu.edu.ua>
2026-03-25 18:14:31 +0200

    Add solution and tests screenshot for Swap Node Pairs In Linked List

---
+++ Swap-Node-Pairs-In-Linked-List/solution.py
@@ -0,0 +1,14 @@
+from preloaded import Node
+
+def swap_pairs(head):
+    dummy = Node(0)
+    dummy.next = head
+    prev = dummy
+    while prev.next and prev.next.next:
+        first = prev.next
+        second = prev.next.next
+        first.next = second.next
+        second.next = first
+        prev.next = second
+        prev = first
+    return dummy.next
